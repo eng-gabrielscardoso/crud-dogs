a0f73a0f6391d1fa254e308a884f19647e8b335e
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2025-02-06 15:48:29 -0300

    add dotenv configuration and example environment file

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,8 @@
 import Hapi from '@hapi/hapi';
+import * as dotenv from 'dotenv-safe';
 import { routes } from './routes';
+
+dotenv.config();
 
 const server = Hapi.server({
   port: process.env.PORT || 80,
